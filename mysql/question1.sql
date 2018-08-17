7c6354defc0bf7c51fa7b1e47d1a0dcf9dd9c575
Wenbo Ma <wenbo.ma@foxmail.com>
2018-08-17 19:12:11 -0400

    Add files via upload

---
+++ mysql/question1.sql
@@ -0,0 +1,14 @@
+select user_id, previous_time
+	from
+	(select user_id, time_stamp,
+		LAG(time_stamp,1) over (partition by user_id order by time_stamp) as previous_time,
+		row_number() over (partition by user_id order by time_stamp desc) as order_desc
+	from q1) tmp
+where order_desc = 1
+order by user_id
+limit 10;
+
+#select *
+#from q1;
+
+# alter table q1 change column timstamp time_stamp integer;
